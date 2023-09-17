6ba7ecdc6777bca1110fbacc37d370678bc20778
kush2863 <70647570+kush2863@users.noreply.github.com>
2023-09-17 18:03:19 +0530

    first commit

--- src/cards.jsx
+++ src/cards.jsx
@@ -3,12 +3,12 @@ import Avatar from "./avatar"; // Make sure the correct path to the Avatar compo
 import Detail from "./detail";
 
 
-const Card = (props) => {
-  const [isFlipped, setIsFlipped] = useState(false);
+  const Card = (props) => {
+    const [isFlipped, setIsFlipped] = useState(false);
 
-  const handleCardClick = () => {
-    setIsFlipped(!isFlipped);
-  };
+    const handleCardClick = () => {
+      setIsFlipped(!isFlipped);
+    };
 
   return (
     <div className="card-container">
@@ -35,3 +35,49 @@ const Card = (props) => {
 };
 
 export default Card;
+const Events = () => {
+  return (
+    <section class="Events event-bg">
+    <div class="Page-Header center-content">
+      <h2>Upcoming and Ongoing Events</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus ea accusamus vitae nulla, ex itaque error nobis ut qui velit. Vero iusto voluptate quis temporibus quas inventore odit nam enim!</p>
+    </div>
+    <div class="Events_Warpper">
+
+      <div class="center-div">
+        <div class="Session_Cards">
+
+          {
+            eventList.map((item,index)=>{
+              return(
+                
+                <div class="Session_Card" key={index}>
+                <img src={image1} alt="session1" />
+                <div className='card-front'> 
+                  <p class="Title">{item.title}</p>
+                  <p class="Date-Time">{item.date}</p>
+                  <p class="Date-Time">{item.time}</p>
+                  </div>
+                  <div className='card-back'>
+                  <p class="Card-text">
+                    {item.text}
+                  </p>
+                  
+                  <button class="btn-1 Session_btn"><a href='{item.link}'></a>Register Now</button>
+                  </div>
+              </div> 
+             
+              )
+            })
+          }
+               
+        </div>
+      </div>
+      </div>
+      
+  </section>
+  )
+}
+
+export default Events;
+
